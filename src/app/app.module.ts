3f3af21f86f337d9adbe97f89225e62e1d5b9cb8
Sinisa Petkovic <sini.petkovic@gmail.com>
2019-05-18 22:00:45 -0400

    add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,9 @@ import { PostsComponent } from './components/posts/posts.component';
 import { UserService } from './services/user.service';
 import { PostService } from './services/post.service';
 import { PostFormComponent } from './components/post-form/post-form.component';
+import { HomeComponent } from './components/home/home.component';
+import { PostComponent } from './components/post/post.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +25,10 @@ import { PostFormComponent } from './components/post-form/post-form.component';
     UsersComponent,
     NavbarComponent,
     PostsComponent,
-    PostFormComponent
+    PostFormComponent,
+    HomeComponent,
+    PostComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
